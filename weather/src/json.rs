d0354ab5c2aec9b4355a8ab18828a713e52fde66
bqoul <77885468+bqoul@users.noreply.github.com>
2021-03-27 15:12:07 +0200

    Create json.rs

---
+++ weather/src/json.rs
@@ -0,0 +1,25 @@
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Forecast {
+    pub location: Location,
+    pub current: Current,
+}
+
+#[derive(Deserialize)]
+pub struct Location {
+    pub name: String,
+    pub country: String,
+}
+
+#[derive(Deserialize)]
+pub struct Current {
+    pub temp_c: f64,
+    pub temp_f: f64,
+    pub condition: Condition,
+}
+
+#[derive(Deserialize)]
+pub struct Condition {
+    pub text: String,
+}

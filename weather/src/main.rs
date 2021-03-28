2af1e6633ab808ec26df0a2eabfa0fcd13f5d092
bqoul <77885468+bqoul@users.noreply.github.com>
2021-03-28 15:04:09 +0300

    Update main.rs

--- weather/src/main.rs
+++ weather/src/main.rs
@@ -2,7 +2,7 @@ mod json;
 
 use std::io::{self, Write};
 use reqwest::Error;
-use json::Forecast;
+use json::{Forecast, Failed};
 use colored::*;
 
 #[tokio::main]
@@ -11,30 +11,48 @@ async fn main() {
         let city = input("\nenter city name: ");
 
         let link = format!("http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}",
-                           api_key = "NOIDONTTHINKSO", //if you want to use this program you'l have to put a proper api-key from weatherapi.com in here
+                           api_key = "9fa274efc5f1414b91d115315212803", 
                            city = city.trim());
 
-        match weather(link).await {
+        match weather(&link).await {
             Ok(forecast) => {
                 println!("weather in {city}({country}): {weather} | temperature: {c}°C ({f}°F)\n",
                     city = forecast.location.name,
                     country = forecast.location.country,
                     weather = forecast.current.condition.text.black().on_white(), 
                     c = forecast.current.temp_c.to_string().black().on_white(),
-                    f = forecast.current.temp_f.to_string().black().on_white())
+                    f = forecast.current.temp_f.to_string().black().on_white());
             },
-            Err(_) => println!("{}", " cant find this city ".black().on_red()),
+            Err(_) => {
+                match weather_failed(&link).await {
+                    Ok(failed) => {
+                        println!("{}", failed.error.message.black().on_red())
+                    },
+                    Err(_) => println!("{}", "No internet connection.".black().on_red()),
+                }
+            }
         }
     }
 }
 
-async fn weather(link: String) -> Result<Forecast, Error> {
+async fn weather(link: &String) -> Result<Forecast, Error> {
+    let link = link.clone();
     let forecast = reqwest::get(&link)
         .await?
         .json::<Forecast>()
         .await?;
 
     Ok(forecast)
+}
+
+async fn weather_failed(link: &String) -> Result<Failed, Error> {
+    let link = link.clone();
+    let failed = reqwest::get(&link)
+        .await?
+        .json::<Failed>()
+        .await?;
+
+    Ok(failed)
 }
 
 fn input(message: &str) -> String {
